5fa3e9d6e5746fe94e945a04e2675f040e90e329
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-10 15:53:47 +0300

    create predictPersonalInfo() to predict the personal information  of user

--- javascript/js.code.js
+++ javascript/js.code.js
@@ -1,6 +1,9 @@
-document.getElementById('button').addEventListener('click', predictAge);
+fetch('https://dog.ceo/api/breeds/image/random');
 
-function predictAge() {
+
+document.getElementById('button').addEventListener('click', predictPersonalInfo);
+
+function predictPersonalInfo() {
     let name = document.getElementById('name')
 
     // //First fetch() to guess the age
@@ -8,36 +11,13 @@ function predictAge() {
         .then(response => response.json())
         .then(data => {
             if (name != '') {
-                age.innerHTML = "I guess your age is " + data.age;
+                age.innerHTML = "I guess your age is " + data.age + ",";
                 console.log(data.age);
             } else {
                 age.style.display = 'none';
             }
         })
 
-    // //Second fetch() to guess the gender
-    fetch('https://api.genderize.io?name=' + name.value)
-        .then(response => response.json())
-        .then(data => {
-            if (name != '') {
-                gender.innerHTML = "I guess your gender is " + data.gender;
-                console.log(data.gender);
-            } else {
-                gender.style.display = 'none';
-            }
-        })
 
-    //Third fetch() to guess the nationality
-    fetch('https://api.nationalize.io/?name=' + name.value)
-        .then(response => response.json())
-        .then(data => {
-            if (name != '') {
-                nationality.innerHTML = "I guess your nationality is " + data.country[0].country_id + " or " + data.country[1].country_id;
-                console.log(data.nationality);
-
-            } else {
-                nationality.style.display = 'none';
-            }
-        })
 
 };
