781f934731a49acbd9ca48b4d9f3699c73ee36a9
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-10 15:01:45 +0300

    create the first function to predict the nationality

--- javascript/js.code.js
+++ javascript/js.code.js
@@ -3,7 +3,7 @@ document.getElementById('button').addEventListener('click', predictAge);
 function predictAge() {
     let name = document.getElementById('name')
 
-    //First fetch() to guess the age
+    // //First fetch() to guess the age
     fetch('https://api.agify.io/?name=' + name.value)
         .then(response => response.json())
         .then(data => {
@@ -15,15 +15,28 @@ function predictAge() {
             }
         })
 
-    //Second fetch() to guess the gender
+    // //Second fetch() to guess the gender
     fetch('https://api.genderize.io?name=' + name.value)
         .then(response => response.json())
         .then(data => {
             if (name != '') {
                 gender.innerHTML = "I guess your gender is " + data.gender;
-                console.log(data.age);
+                console.log(data.gender);
             } else {
                 gender.style.display = 'none';
+            }
+        })
+
+    //Third fetch() to guess the nationality
+    fetch('https://api.nationalize.io/?name=' + name.value)
+        .then(response => response.json())
+        .then(data => {
+            if (name != '') {
+                nationality.innerHTML = "I guess your nationality is " + data.country[0].country_id + " or " + data.country[1].country_id;
+                console.log(data.nationality);
+
+            } else {
+                nationality.style.display = 'none';
             }
         })
 
