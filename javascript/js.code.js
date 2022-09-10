03e17193ff7d4b7c32581048b97657f86bb81f12
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-10 14:10:04 +0300

    create the second  function to guess the gender

--- javascript/js.code.js
+++ javascript/js.code.js
@@ -14,4 +14,17 @@ function predictAge() {
                 age.style.display = 'none';
             }
         })
+
+    //Second fetch() to guess the gender
+    fetch('https://api.genderize.io?name=' + name.value)
+        .then(response => response.json())
+        .then(data => {
+            if (name != '') {
+                gender.innerHTML = "I guess your gender is " + data.gender;
+                console.log(data.age);
+            } else {
+                gender.style.display = 'none';
+            }
+        })
+
 };
